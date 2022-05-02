a5cd7203e688df44388ee18abd48a26775eb789e
Giovane Sedano <35101491+giovaness30@users.noreply.github.com>
2022-05-02 18:23:26 -0300

    api Online

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: `http://localhost:3000/`,
+  baseURL: `https://my-json-server.typicode.com/giovaness30/myjsonserver/`,
   headers: {
     'Content-Type': 'application/json'
   }
